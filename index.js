cb7a51e625cf028e0c9f847ccd966854c81d2bf5
Mou-Chun Wang <jimmyexx@gmail.com>
2018-01-22 23:40:41 +0800

    Try01- See if Google works

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log('starting function')
 
 var fs = require('fs');
 var readline = require('readline');
-var googleApi = require('googleapis');
+var google = require('googleapis');
 var googleAuth = require('google-auth-library');
 
 exports.handler = function index(event, context, callback) {
@@ -13,6 +13,7 @@ exports.handler = function index(event, context, callback) {
     //   "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
     //   "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
     // },
+    //body: { "message": "20180103[1038] - Your Selection is indeed : " + event.type }
     body: { "message": "20180103[1038] - Your Selection is indeed : " + event.type }
   };
 
@@ -27,20 +28,46 @@ exports.handler = function index(event, context, callback) {
   };
 
   const GOOGLE_MAP_API_KEY = 'AIzaSyBY7C54J0Z2tm_OOORmDvVY0gZjeNQIvQY';
-  
 
 
+  //Defined at AWS-Lambda
+  const GSA_CLIENT_EMAIL = process.env['GSA_CLIENT_EMAIL'];
+  const GSA_CLIENT_PRIVATE_KEY = process.env['GSA_CLIENT_PRIVATE_KEY'];
 
 
+    // configure a JWT auth client
+    let jwtClient = new google.auth.JWT(
+        GSA_CLIENT_EMAIL,
+        null,
+        GSA_CLIENT_PRIVATE_KEY,
+        ['https://www.googleapis.com/auth/calendar']);
+    //authenticate request
+    jwtClient.authorize(function (err, tokens) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            //console.log("Successfully connected!");
+        }
+    });
+
+    let calendar = google.calendar('v3');
 
 
+    calendar.events.list({
+        auth: jwtClient,
+        //desired Calendar ID
+        calendarId: CALENDAR_ID['blues'],
+        timeZone: "Asia/Taipei"
+    }, function (err, response) {
 
-  
-
-
-
-
-
+        const NewResponse = {
+            statusCode: 200,
+            body: response
+        };
+        callback(null, NewResponse);
+        return; //Should stop from here
+    }
 
 
   console.log('processing event: %j', event)
