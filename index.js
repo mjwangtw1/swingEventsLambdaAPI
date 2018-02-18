c395e9b77b8636163d4b1c9a9cfe7b0d91c9f73d
Mou-Chun Wang <jimmyexx@gmail.com>
2018-02-18 18:56:24 +0800

    missed the dt

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var google = require('googleapis');
 
 exports.handler = function index(event, context, callback) {
 
+    var dt = new Date();
+    dt.setDate(dt.getDate()+7);
+    nextWeek = dt.toLocaleString(['zh-TW'],{timeZone: 'Asia/Taipei'});
+
+    var nowdt = new Date();
+    today = nowdt.toLocaleString(['zh-TW'],{timeZone: 'Asia/Taipei'});
+
   const response = {
     statusCode: 200,
     body: { "message": "20180218[1429] - Your Selection is indeed : " + event.type }
@@ -16,14 +23,16 @@ exports.handler = function index(event, context, callback) {
 
   //const GOOGLE_MAP_API_KEY = 'AIzaSyBY7C54J0Z2tm_OOORmDvVY0gZjeNQIvQY';
 
-  //Defined at AWS-Lambda
-
    var Buffer = require('buffer').Buffer;
-   var PRIVATE_KEY = new Buffer(process.env['GSA_CLIENT_PRIVATE_KEY'], 'base64').toString();
+
+   //Defined at AWS-Lambda
+   //Data from Lambda is Based_64, need to convert back
+   const CLIENT_EMAIL = process.env['GSA_CLIENT_EMAIL'];
+   const PRIVATE_KEY = new Buffer(process.env['GSA_CLIENT_PRIVATE_KEY'], 'base64').toString();
 
     // configure a JWT auth client
     let jwtClient = new google.auth.JWT(
-        process.env['GSA_CLIENT_EMAIL'],
+        CLIENT_EMAIL,
         null,
         PRIVATE_KEY,
         ['https://www.googleapis.com/auth/calendar']);
@@ -36,7 +45,7 @@ exports.handler = function index(event, context, callback) {
     //authenticate request
         jwtClient.authorize(function (err, tokens) {
             if (err) {
-                console.log('JWT auth fail 1846');
+                console.log('JWT auth fail 1855');
                 console.log(err);
                 console.log(tokens);
                 return;
