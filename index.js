70b5aeba47883f84059837207e56076c0d0c3cc8
Mou-Chun Wang <jimmyexx@gmail.com>
2018-02-18 19:55:56 +0800

    npm install async

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var google = require('googleapis');
+var async = require('async');
 
 exports.handler = function index(event, context, callback) {
 
@@ -9,10 +10,12 @@ exports.handler = function index(event, context, callback) {
     var nowdt = new Date();
     today = nowdt.toLocaleString(['zh-TW'],{timeZone: 'Asia/Taipei'});
 
-  const responseMessage = {
-    statusCode: 200,
-    body: { "message": "20180218[1901] - Your Selection is indeed : " + event.type }
-  };
+  // const responseMessage = {
+  //   statusCode: 200,
+  //   body: { "message": "20180218[1901] - Your Selection is indeed : " + event.type }
+  // };
+
+  var displayMessage = "20180218[1921] - Your Selection is indeed : " + event.type;
 
   //user Selection event.type
   const CALENDAR_ID = {
@@ -30,59 +33,84 @@ exports.handler = function index(event, context, callback) {
    const CLIENT_EMAIL = process.env['GSA_CLIENT_EMAIL'];
    const PRIVATE_KEY = new Buffer(process.env['GSA_CLIENT_PRIVATE_KEY'], 'base64').toString();
 
-    // configure a JWT auth client
-    let jwtClient = new google.auth.JWT(
-        CLIENT_EMAIL,
-        null,
-        PRIVATE_KEY,
-        ['https://www.googleapis.com/auth/calendar']);
+   //We use Async
+   async.waterfall([
 
-    // console.log('checking private_key');
-    // console.log(process.env['GSA_CLIENT_PRIVATE_KEY']);
-    // console.log(jwtClient);
-    // return;
+       function auth(next){
+
+           // configure a JWT auth client
+           let jwtClient = new google.auth.JWT(
+               CLIENT_EMAIL,
+               null,
+               PRIVATE_KEY,
+               ['https://www.googleapis.com/auth/calendar']
+           );
+
+           jwtClient.authorize(function (err, tokens){
+                next(err,tokens);
+           });
+
+       },function fetchCalendar(result1, next){
+
+           let calendar = google.calendar('v3');
+
+           calendar.events.list({
+               auth: jwtClient,
+               calendarId: CALENDAR_ID['blues'], //desired Calendar ID
+               timeZone: "Asia/Taipei",
+               //TimeMax: dt
+           }, function (err, response) {
+
+               next(err,response);
+           }
+       }
+
+   ],function (err,result){
+
+       //err Handling
+       if(err) throw err;
+       callback(null, result);
+   });
+
+
 
     //authenticate request
-        jwtClient.authorize(function (err, tokens) {
-            if (err) {
-                console.log('JWT auth fail 1855');
-                console.log(err);
-                console.log(tokens);
-                return;
-            } else {
-                //console.log('JWT auth works 1846');
-                //console.log("Successfully connected!");
-            }
-        });
-        //console.log(jwtClient);
+    //     jwtClient.authorize(function (err, tokens) {
+    //         if (err) {
+    //             console.log('JWT auth fail 1855');
+    //             console.log(err);
+    //             console.log(tokens);
+    //             return;
+    //         } else {
+    //
+    //         }
+    //     });
 
-    let calendar = google.calendar('v3');
+    // let calendar = google.calendar('v3');
+    //
+    // calendar.events.list({
+    //     auth: jwtClient,
+    //     calendarId: CALENDAR_ID['blues'], //desired Calendar ID
+    //     timeZone: "Asia/Taipei",
+    //     //TimeMax: dt
+    // }, function (err, response) {
+    //
+    //     console.log(response);
+    //
+    //     var NewResponse = {
+    //         statusCode: 202,
+    //         body: response
+    //         // body: {
+    //         //   "Desc" : response.description,
+    //         //   "timeZone" : response.timeZone,
+    //         //   "items" : response.items
+    //         // }
+    //     };
+    //     callback(null, NewResponse);
+    //     return; //Should stop from here
+    // });
 
-    calendar.events.list({
-        auth: jwtClient,
-        //desired Calendar ID
-        calendarId: CALENDAR_ID['blues'],
-        timeZone: "Asia/Taipei",
-        //TimeMax: dt
-    }, function (err, response) {
+    console.log('processing event: %j', event)
 
-        console.log(response);
-
-        var NewResponse = {
-            statusCode: 202,
-            body: response
-
-            // body: {
-            //   "Desc" : response.description,
-            //   "timeZone" : response.timeZone,
-            //   "items" : response.items
-            // }
-        };
-        callback(null, NewResponse);
-        return; //Should stop from here
-    });
-
-  console.log('processing event: %j', event)
-
-  callback(null, responseMessage);
+    callback(null, responseMessage);
 }
