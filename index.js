c0a386beff3c3f0ccd9527abc619954fefb05142
Mou-Chun Wang <jimmyexx@gmail.com>
2018-02-18 15:21:45 +0800

    now checking if the calendar stuff fail

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ exports.handler = function index(event, context, callback) {
                 console.log(err);
                 return;
             } else {
+                console.log('JWT auth works');
                 //console.log("Successfully connected!");
             }
         });
@@ -51,29 +52,32 @@ exports.handler = function index(event, context, callback) {
 
     let calendar = google.calendar('v3');
 
-    calendar.events.list({
-        auth: jwtClient,
-        //desired Calendar ID
-        calendarId: CALENDAR_ID['blues'],
-        timeZone: "Asia/Taipei",
-        TimeMax: dt
-    }, function (err, response) {
+    console.log('Stop here');
 
-        console.log('error?');
-        console.log(err);
-        exit();
 
-        var NewResponse = {
-            statusCode: 200,
-            body: {
-              "Desc" : response.description,
-              "timeZone" : response.timeZone,
-              "items" : response.items
-            }
-        };
-        callback(null, NewResponse);
-        return; //Should stop from here
-    });
+    // calendar.events.list({
+    //     auth: jwtClient,
+    //     //desired Calendar ID
+    //     calendarId: CALENDAR_ID['blues'],
+    //     timeZone: "Asia/Taipei",
+    //     TimeMax: dt
+    // }, function (err, response) {
+    //
+    //     console.log('error?');
+    //     console.log(err);
+    //     exit();
+    //
+    //     var NewResponse = {
+    //         statusCode: 200,
+    //         body: {
+    //           "Desc" : response.description,
+    //           "timeZone" : response.timeZone,
+    //           "items" : response.items
+    //         }
+    //     };
+    //     callback(null, NewResponse);
+    //     return; //Should stop from here
+    // });
 
   console.log('processing event: %j', event)
 
