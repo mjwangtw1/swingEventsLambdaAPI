b03e56a418eeb4ddaa85e0b330dfc5355ff3f9bb
Unknown <mou.wang@104.com.tw>
2018-04-04 12:24:11 +0800

    Now we put back the Selections. (swing/blues/Primary)

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-console.log('starting function')
+console.log('starting function - 20180405')
 
 var google = require('googleapis');
 
 exports.handler = function index(event, context, callback) {
+
+    //Here Deals with the time format
+    var dt = new Date();
+    dt.setDate(dt.getDate()+7);
+    nextWeek = dt.toLocaleString(['zh-TW'],{timeZone: 'Asia/Taipei'});
+
+    var nowdt = new Date();
+    today = nowdt.toLocaleString(['zh-TW'],{timeZone: 'Asia/Taipei'});
+
+    timestring = ', Nowdate: ' + nowdt + ' , NextWeekDt : ' + nextWeek;
 
     var response = {
         statusCode: 200,
@@ -11,18 +21,34 @@ exports.handler = function index(event, context, callback) {
         //   "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
         // },
         //body: { "message": "20180103[1038] - Your Selection is indeed : " + event.type }
-        body: { "message": "20180218[2252] - Your Selection is indeed : " + event.type }
+        body: { "message": "20180218[2252] - Your Selection is indeed : " + event.type + timestring }
     };
 
     //user Selection event.type
-
     //Get the current Date:
+    // const CALENDAR_ID = {
+    //     'primary': 'k89l8gcv9l19k5aafaolmn2d38@group.calendar.google.com', //Special Events
+    //     'swing' : 'du5ncgcem4duked6jui8p1g5as@group.calendar.google.com',
+    //     'blues' : 'hbcpknmo5l1jp455qdbrjps2uo@group.calendar.google.com'
+    // };
 
-    const CALENDAR_ID = {
-        'primary': 'k89l8gcv9l19k5aafaolmn2d38@group.calendar.google.com', //Special Events
-        'swing' : 'du5ncgcem4duked6jui8p1g5as@group.calendar.google.com',
-        'blues' : 'hbcpknmo5l1jp455qdbrjps2uo@group.calendar.google.com'
-    };
+    const PRIMARY_CALENDAR = 'k89l8gcv9l19k5aafaolmn2d38@group.calendar.google.com';
+    const SWING_CALENDAR = 'du5ncgcem4duked6jui8p1g5as@group.calendar.google.com';
+    const BLUES_CALENDAR = 'hbcpknmo5l1jp455qdbrjps2uo@group.calendar.google.com';
+
+    targetCalendar = PRIMARY_CALENDAR; //If besides swing/blues, we go for this one
+
+    switch(event.type){
+        case 'swing':
+            targetCalendar = SWING_CALENDAR;
+            break;
+
+        case 'blues':
+            tagetCalendar = BLUES_CALENDAR;
+            break;
+    }
+
+
 
     //const GOOGLE_MAP_API_KEY = 'AIzaSyBY7C54J0Z2tm_OOORmDvVY0gZjeNQIvQY';
 
@@ -52,7 +78,8 @@ exports.handler = function index(event, context, callback) {
     calendar.events.list({
         auth: jwtClient,
         //desired Calendar ID
-        calendarId: CALENDAR_ID['blues'],
+        calendarId: targetCalendar,
+        //calendarId: CALENDAR_ID['blues'],
         timeZone: "Asia/Taipei"
     }, function (err, res) {
         if (err) {
